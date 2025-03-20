710c42bb070d52fd186ce892fac1d88a13c87b75
ayoubediri <ayoubediri2001@gmail.com>
2025-03-20 17:59:47 +0000

    ex01 resource

--- starting/ex01/format_ft_time.py
+++ starting/ex01/format_ft_time.py
@@ -1,9 +1,20 @@
 import datetime
 import time
 
+# First I need to get seconds from 1 1 1970. and formatted to be like this 1,000,000 
+# that why I use time.time to get it the time and formatted to be like what we want, 
+# the method 'format' is the key to that
+
 time_in_seconds = "{:,}".format(time.time())
+
+# I do the same thing to convert the seconds to the scientific format
+
 time_in_scientific = "{:.2e}".format(time.time())
+
+# and now I get the current time
 now = datetime.datetime.now()
+
+# and formatted to be like { month shortcut, day in number, years}
 tody_date = now.strftime("%b %d %Y")
 print(f"Seconds since January 1, 1970: {time_in_seconds} or {time_in_scientific} in scientific notation")
 print(tody_date)
