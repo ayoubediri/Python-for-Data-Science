ece409903a1d8545d48c13dc54d70f654ad6617f
ayoubediri <ayoubediri2001@gmail.com>
2025-03-20 16:45:24 +0000

    ex00 and sample documentation for it

--- starting/ex00/Hello.py
+++ starting/ex00/Hello.py
@@ -2,7 +2,7 @@ ft_list = ["Hello", "World!"]
 ft_tuple = ("Hello", "Morocco!")
 ft_set = {"Rabat!", "Hello"}
 ft_dict = {"Hello" : "1337!"}
-#your code here
+
 print(ft_list)
 print(ft_tuple)
 print(ft_set)
