d2badfad5723893b50a8f160b3767a9828e28f6c
ayoubediri <ayoubediri2001@gmail.com>
2025-03-26 02:57:32 +0000

    ex02

---
+++ starting/ex02/find_ft_type.py
@@ -0,0 +1,17 @@
+def all_thing_is_obj(object: any) -> int:
+    type_map = {
+        list: "List",
+        dict: "Dict",
+        tuple: "Tuple",
+        set: "Set",
+        str: "is in the kitchen",
+    }
+    obj_type = type(object)
+    if obj_type in type_map:
+        if (obj_type == str):
+            print(f"{object} {type_map[str]} : {obj_type}")
+        else :
+            print(f"{type_map[obj_type]} : {obj_type}")
+    else :
+        print("Type not found")
+    return 42
---
+++ starting/ex02/readme.md
@@ -0,0 +1,3 @@
+visit this link for basic knowledge about functions:
+
+https://peppered-mine-9af.notion.site/Function-in-Python-1bc4665ba38a80b3b0a9cedff2eee89f
