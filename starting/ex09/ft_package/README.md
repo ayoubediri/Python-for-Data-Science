a399f7a2f8fb4a35253e286566bcdb13b5b71179
ayoubediri <ayoubediri2001@gmail.com>
2025-05-13 11:14:59 +0100

    v0.1

---
+++ starting/ex09/ft_package/README.md
@@ -0,0 +1,11 @@
+# ft_package
+
+A sample test package containing a single function: `count_in_list`.
+
+## Installation
+
+To install the package, you can use:
+
+```bash
+pip install ./dist/ft_package-0.0.1.tar.gz
+```
---
+++ starting/ex09/ft_package/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ft_package',
+    version='0.0.1',
+    packages=find_packages(),
+    description='A sample test package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='adiri',
+    author_email='adiri@student.1337.ma',
+    url='https://github.com/eagle/ft_package',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
