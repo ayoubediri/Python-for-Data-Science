d4de3fd3b6d1faccc0b2ca87ba03190de4d7c5cf
ayoubediri <ayoubediri2001@gmail.com>
2025-05-18 18:42:27 +0100

    ex01

---
+++ array/ex01/array2D.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def check_uneven_lists(arr : list) -> bool:
+    lengths = [len(sublist) for sublist in arr]
+    return len(set(lengths)) > 1
+
+def slice_me(family: list, start: int, end: int) -> list:
+        if (type(family) != list or
+            type(start) != int or
+            type(end) != int):
+            raise TypeError("family must be a list, start and end must be integers")
+        if check_uneven_lists(family):
+                raise ValueError("All sublists must have the same length")
+        arr = np.array(family)
+        print(f"My shape is : {arr.shape}")
+        arr = arr[start:end]
+        print(f"My new shape is : {arr.shape}")
+        return arr.tolist()
