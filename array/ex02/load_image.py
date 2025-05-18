b5c23f991b5ae519ac9c658604bc565a61fffb5c
ayoubediri <ayoubediri2001@gmail.com>
2025-05-18 22:14:24 +0100

    ex02

---
+++ array/ex02/load_image.py
@@ -0,0 +1,14 @@
+import numpy as np
+from PIL import Image
+def ft_load(path: str) -> np.ndarray:
+    try:
+        img = Image.open(path)
+        if (img.mode != 'RGB'):
+            img = img.convert('RGB')
+        img_array = np.array(img)
+        print(f"{img_array.shape}")
+        return img_array
+    except FileNotFoundError:
+        print("Error: Image file not found. Please check the path.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
