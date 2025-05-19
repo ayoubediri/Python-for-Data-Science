e8629a27c944947687d7b8369fc1ce7b927e95a6
ayoubediri <ayoubediri2001@gmail.com>
2025-05-19 09:49:18 +0100

    ex04

--- array/ex04/rotate.py
+++ array/ex04/rotate.py
@@ -2,18 +2,20 @@ from load_image import ft_load
 from PIL import Image
 import numpy as np
 
-def ft_zoom(path : str) :
+def ft_rotate(path : str) :
     img = ft_load(path)
-    print(f"The shape of image is: {img.shape}")
-    print(img)
     img = img[-600:-200, -500:-100]
     image = Image.fromarray(img)
     image = image.convert('L')
     img_array = np.array(image)
     img_array = np.expand_dims(img_array, axis=-1)
-    print(f"New shape after slicing: {img_array.shape}")
+    print(f"The shape of image is: {img_array.shape}")
     print(img_array)
+    image = image.transpose(Image.ROTATE_90)
+    image_array = np.array(image)
+    print(f"New shape after Transpose: {image_array.shape}")
+    print(image_array)
     image.show()
 
-if __name__ == "__main__":
-    ft_zoom("animal.jpeg")
+if __name__ == "__main__" :
+    ft_rotate("animal.jpeg")
